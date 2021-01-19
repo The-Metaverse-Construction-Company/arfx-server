c7bcb33e857aa34ac1eccee3c79b6726883990de
Darryl Fabian <lalekidman@gmail.com>
2021-01-19 16:17:18 +0800

    done on initial setup for the project

--- packages/server/boilerplate/src/api/domain/entities/index.ts
+++
@@ -1,9 +0,0 @@
-import {v4 as uuidV4} from 'uuid'
-import bcrypt from 'bcryptjs'
-import User from './users'
-
-
-export const UserEntity = User({generateId: uuidV4, hash: (pwd: string) => {
-  const hashPassword = bcrypt.hashSync(pwd, 10)
-  return hashPassword
-}})
--- packages/server/boilerplate/src/api/domain/entities/users/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ALLOWED_USER_ROLE =  {
-  USER: 'user',
-  ADMIN: 'admin'
-}
