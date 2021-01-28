0502a0a0781a790eecbb012e1c935d3a638c64f9
Darryl Fabian <lalekidman@gmail.com>
2021-01-28 21:05:42 +0800

    done implement user product swagg

--- src/api/routes/v1/user-products.route.ts
+++ src/api/routes/v1/user-products.route.ts
@@ -7,9 +7,42 @@ import * as validations from '../../validations/purchase-history.validation'
 const router = express.Router();
 
 router.route('/')
+// dummy endpoint
+//x only for testing
   .post(controller.createUserProductRoute)
+/**
+ * @swagger
+ * /v1/users/{userId}/products:
+ *  patch:
+ *    summary: "List of product/scene that purchased of the user/customer."
+ *    tags:
+ *      - "User-Products"
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - $ref: '#/components/requestParams/Product/id'
+ *    responses:
+ *      '200':
+ *        $ref: '#/components/responses/UserProducts'
+ */
   .get(controller.userProductListRoute)
 router.route('/:userProductId')
+/**
+ * @swagger
+ * /v1/users/{userId}/products/{productId}:
+ *  patch:
+ *    summary: "Details of product/scene that purchased of the user/customer."
+ *    tags:
+ *      - "User-Products"
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - $ref: '#/components/requestParams/User/id'
+ *      - $ref: '#/components/requestParams/Product/id'
+ *    responses:
+ *      '200':
+ *        $ref: '#/components/responses/UserProduct'
+ */
   .get(controller.userProductDetailsRoute)
 
 export default router;
--- src/api/routes/v1/user.route.ts
+++ src/api/routes/v1/user.route.ts
@@ -46,28 +46,19 @@ router
 router.param('userId', controller.load);
 router
   .route('/')
-  /**
-   * @api {get} v1/users List Users
-   * @apiDescription Get a list of users
-   * @apiVersion 1.0.0
-   * @apiName ListUsers
-   * @apiGroup User
-   * @apiPermission admin
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiParam  {Number{1-}}         [page=1]     List page
-   * @apiParam  {Number{1-100}}      [perPage=1]  Users per page
-   * @apiParam  {String}             [name]       User's name
-   * @apiParam  {String}             [email]      User's email
-   * @apiParam  {String=user,admin}  [role]       User's role
-   *
-   * @apiSuccess {Object[]} users List of users.
-   *
-   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
-   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
-   */
-  // .get(validate(listUsers), controller.list)
+/**
+ * @swagger
+ * /v1/users:
+ *  patch:
+ *    summary: "List of the user/customer."
+ *    tags:
+ *      - "Users"
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      '200':
+ *        $ref: '#/components/responses/User/List'
+ */
   .get(authorizeAdminAccount(), validate(listUsers), controller.list)
   // .get(authorize(ALLOWED_USER_ROLE.ADMIN), validate(listUsers), controller.list)
   /**x
@@ -99,125 +90,32 @@ router
   .post(authorize(ALLOWED_USER_ROLE.ADMIN), validate(createUser), controller.create);
 
 
-router
-  .route('/profile')
-  /**
-   * @api {get} v1/users/profile User Profile
-   * @apiDescription Get logged in user profile information
-   * @apiVersion 1.0.0
-   * @apiName UserProfile
-   * @apiGroup User
-   * @apiPermission user
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiSuccess {String}  id         User's id
-   * @apiSuccess {String}  name       User's name
-   * @apiSuccess {String}  email      User's email
-   * @apiSuccess {String}  role       User's role
-   * @apiSuccess {Date}    createdAt  Timestamp
-   *
-   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated Users can access the data
-   */
-  .get(authorize(), controller.loggedIn);
+// router
+//   .route('/profile')
+//   .get(authorize(), controller.loggedIn);
 
 
 router
   .route('/:userId')
-  /**
-   * @api {get} v1/users/:id Get User
-   * @apiDescription Get user information
-   * @apiVersion 1.0.0
-   * @apiName GetUser
-   * @apiGroup User
-   * @apiPermission user
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiSuccess {String}  id         User's id
-   * @apiSuccess {String}  name       User's name
-   * @apiSuccess {String}  email      User's email
-   * @apiSuccess {String}  role       User's role
-   * @apiSuccess {Date}    createdAt  Timestamp
-   *
-   * @apiError (Unauthorized 401) Unauthorized Only authenticated users can access the data
-   * @apiError (Forbidden 403)    Forbidden    Only user with same id or admins can access the data
-   * @apiError (Not Found 404)    NotFound     User does not exist
-   */
+/**x
+ * @swagger
+ * /v1/users/{userId}:
+ *  patch:
+ *    summary: "List of the user/customer."
+ *    tags:
+ *      - "Users"
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - $ref: '#/components/requestParams/User/id'
+ *    responses:
+ *      '200':
+ *        $ref: '#/components/responses/User/Detail'
+ */
   .get(authorize(LOGGED_USER), controller.get)
-  /**
-   * @api {put} v1/users/:id Replace User
-   * @apiDescription Replace the whole user document with a new one
-   * @apiVersion 1.0.0
-   * @apiName ReplaceUser
-   * @apiGroup User
-   * @apiPermission user
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiParam  {String}             email     User's email
-   * @apiParam  {String{6..128}}     password  User's password
-   * @apiParam  {String{..128}}      [name]    User's name
-   * @apiParam  {String=user,admin}  [role]    User's role
-   * (You must be an admin to change the user's role)
-   *
-   * @apiSuccess {String}  id         User's id
-   * @apiSuccess {String}  name       User's name
-   * @apiSuccess {String}  email      User's email
-   * @apiSuccess {String}  role       User's role
-   * @apiSuccess {Date}    createdAt  Timestamp
-   *
-   * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
-   * @apiError (Unauthorized 401) Unauthorized Only authenticated users can modify the data
-   * @apiError (Forbidden 403)    Forbidden    Only user with same id or admins can modify the data
-   * @apiError (Not Found 404)    NotFound     User does not exist
-   */
-  .put(authorize(LOGGED_USER), validate(replaceUser), controller.replace)
-  /**
-   * @api {patch} v1/users/:id Update User
-   * @apiDescription Update some fields of a user document
-   * @apiVersion 1.0.0
-   * @apiName UpdateUser
-   * @apiGroup User
-   * @apiPermission user
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiParam  {String}             email     User's email
-   * @apiParam  {String{6..128}}     password  User's password
-   * @apiParam  {String{..128}}      [name]    User's name
-   * @apiParam  {String=user,admin}  [role]    User's role
-   * (You must be an admin to change the user's role)
-   *
-   * @apiSuccess {String}  id         User's id
-   * @apiSuccess {String}  name       User's name
-   * @apiSuccess {String}  email      User's email
-   * @apiSuccess {String}  role       User's role
-   * @apiSuccess {Date}    createdAt  Timestamp
-   *
-   * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
-   * @apiError (Unauthorized 401) Unauthorized Only authenticated users can modify the data
-   * @apiError (Forbidden 403)    Forbidden    Only user with same id or admins can modify the data
-   * @apiError (Not Found 404)    NotFound     User does not exist
-   */
-  .patch(authorize(LOGGED_USER), validate(updateUser), controller.update)
-  /**
-   * @api {patch} v1/users/:id Delete User
-   * @apiDescription Delete a user
-   * @apiVersion 1.0.0
-   * @apiName DeleteUser
-   * @apiGroup User
-   * @apiPermission user
-   *
-   * @apiHeader {String} Authorization   User's access token
-   *
-   * @apiSuccess (No Content 204)  Successfully deleted
-   *
-   * @apiError (Unauthorized 401) Unauthorized  Only authenticated users can delete the data
-   * @apiError (Forbidden 403)    Forbidden     Only user with same id or admins can delete the data
-   * @apiError (Not Found 404)    NotFound      User does not exist
-   */
-  .delete(authorize(LOGGED_USER), controller.remove);
+  // .put(authorize(LOGGED_USER), validate(replaceUser), controller.replace)
+  // .patch(authorize(LOGGED_USER), validate(updateUser), controller.update)
+  // .delete(authorize(LOGGED_USER), controller.remove);
 
 
 export default router;
