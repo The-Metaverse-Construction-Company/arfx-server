dd43b6b95d155cf6c45be9e851c862b606978296
Darryl Fabian <lalekidman@gmail.com>
2021-03-11 21:11:16 +0800

    remove unnecessary codes

--- src/config/azure-ad.ts
+++ src/config/azure-ad.ts
@@ -14,9 +14,6 @@ import {
   AZURE_AD_CLIENT_SECRET_ID
 } from '../config/vars'
 const AZURE_AD_CONFIG = <IBearerStrategyOption> {
-  // identityMetadata: 'https://login.microsoftonline.com/shawmakesmagicgmail.onmicrosoft.com/v2.0/.well-known/openid-configuration', 
-  // identityMetadata: 'https://login.microsoftonline.com/shawmakesmagicgmail.onmicrosoft.com/oauth2/token', 
-  // identityMetadata: 'https://login.microsoftonline.com/shawmakesmagicgmail.onmicrosoft.com/oauth2/v2.0/authorize', 
   identityMetadata: `https://${AZURE_AD_ACCOUNT_NAME}.b2clogin.com/${AZURE_AD_ACCOUNT_NAME}.onmicrosoft.com/B2C_1_SIGN_UP_SIGN_IN1/v2.0/.well-known/openid-configuration`, 
   clientID: AZURE_AD_CLIENT_ID,
   clientSecret: AZURE_AD_CLIENT_SECRET_ID, 
